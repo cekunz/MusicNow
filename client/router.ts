cb4309fc375772b049c3bea1a0834f9fb8776539
bleo <bleo@mit.edu>
2022-11-22 01:05:06 -0500

    added registration page

--- client/router.ts
+++ client/router.ts
@@ -8,6 +8,7 @@ import FindFriendsPage from './components/FindFriends/FindFriendsPage.vue';
 import RegisterPage from './components/Login/RegisterPage.vue';
 import ProfilePage from './components/Profile/ProfilePage.vue';
 import FindFriendsPage from './components/FindFriends/FindFriendsPage.vue';
+import RegisterPage from './components/Login/RegisterPage.vue';
 import NotFound from './NotFound.vue';
 
 Vue.use(VueRouter);
@@ -21,6 +22,7 @@ const routes = [
   {path: '/register', name: 'Register', component: RegisterPage},
   {path: '/profile', name: 'Profile', component: ProfilePage},
   {path: '/findFriends', name: 'Find Friends', component: FindFriendsPage},
+  {path: '/register', name: 'Register', component: RegisterPage},
   {path: '*', name: 'Not Found', component: NotFound}
 ];
 

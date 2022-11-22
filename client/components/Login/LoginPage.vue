6628d1bb033c8ce9f6f5f94aaa6a15853025149d
bleo <bleo@mit.edu>
2022-11-22 00:34:36 -0500

    added login page

--- client/components/Login/LoginPage.vue
+++ client/components/Login/LoginPage.vue
@@ -3,30 +3,57 @@
 
 <template>
   <main>
-    <section>
-      <header>
-        <h2>New to Fritter?</h2>
-      </header>
-      <RegisterForm />
-    </section>
-    <section>
-      <header>
-        <h2>Have an account?</h2>
-      </header>
+    <MusicNowHeader />
+    <article class="forms-container">
       <LoginForm />
-    </section>
+      <section class="signup-section">
+        <p>Dont have an account?</p>
+        <button>Sign Up</button>
+      </section>
+    </article>
   </main>
 </template>
 
 <script>
 import RegisterForm from '@/components/Login/RegisterForm.vue';
 import LoginForm from '@/components/Login/LoginForm.vue';
+import MusicNowHeader from '@/components/common/MusicNowHeader.vue';
 
 export default {
   name: 'LoginPage',
   components: {
     RegisterForm,
-    LoginForm
+    LoginForm,
+    MusicNowHeader
   }
 };
 </script>
+
+<style scoped>
+.forms-container {
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 32%;
+}
+
+.signup-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid rgb(176, 176, 176);
+  border-radius: 4px;
+  padding: 1rem 4rem;
+  padding-bottom: 2rem;
+  margin-top: 8px;
+}
+
+button {
+  font-size: 15px;
+  padding: 4px;
+  margin-top: 8px;
+  height: 2em;
+  width: 100%;
+}
+</style>

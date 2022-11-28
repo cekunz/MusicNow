6f37473e641f3017b4bc10b8fd6ed92f071c1831
Callie <cekunz@mit.edu>
2022-11-28 17:11:43 -0500

    reset mixtape every time a user logs in

--- client/App.vue
+++ client/App.vue
@@ -22,6 +22,7 @@ export default {
       .then((res) => {
         const user = res.user;
         this.$store.commit('setUsername', user ? user.username : null);
+        this.$store.commit('resetMixtape');
       });
 
     // Clear alerts on page refresh
